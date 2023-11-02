d210126ecfbc10cbf18d8b61589494c22faab1b2
unknown <anna.anass@gmail.com>
2023-11-02 21:33:45 +0200

    curs18 final

--- src/main/java/tests/XPathExample.java
+++ src/main/java/tests/XPathExample.java
@@ -1,6 +1,8 @@
 package tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import utils.BaseTest;
@@ -20,6 +22,37 @@ public class XPathExample extends BaseTest{
 		driver.findElement(By.xpath("//input[@class='submit_button']")).click();
 		
 		Thread.sleep(4000);
+		
+	}
+	
+	@Test(priority=2)
+	public void xpathExample2() {
+		
+		driver.findElement(By.xpath("//span[@class='user_name']")).click();
+		
+		// text --> citeste textul dintre tag-urile HTML
+		// <a href="https://keybooks.ro/wp-admin/profile.php" class="icon icon-cog">Settings</a>
+		driver.findElement(By.xpath("//a[text() = 'Settings']")).click();
+		
+		//contains()
+		// //a[contains(text(), 'recent')] --> referinta catre textul dintre tag-uri cu text()
+		//  //a[contains(@href, "orders")] --> referinta catre atributul href cu @href
+		driver.findElement(By.xpath("//a[contains(text(), 'recent')]")).click();
+		
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		
+		//direct child --> /
+		WebElement oderTableHeader = driver.findElement(By.xpath("//th[contains(@class, 'woocommerce-orders-table__header')]/span[text()='Order']"));
+		jse.executeScript("arguments[0].setAttribute('style', 'border:4px solid red')", oderTableHeader);
+		
+		//index based
+		WebElement totalTableHeader = driver.findElement(By.xpath("(//th[contains(@class, 'woocommerce-orders-table__header')]/span)[1]"));
+		jse.executeScript("arguments[0].setAttribute('style', 'border:4px solid orange')", totalTableHeader);
+		
+		WebElement orderTableHeader = driver.findElement(By.xpath("//td[contains(@class, 'woocommerce-orders-table__cell')][@data-title='Order']/a[not(contains(text(), '#172' ))]"));
+		orderTableHeader.click();
+		
+		
 		
 	}
 
